f7775b71b125f9b0b01713bf6bb8c967d0424f90
parag-radadiya <parag.180410107091@gmail.com>
2023-08-31 12:04:46 +0530

    change in auth related APIS in registration

--- services/user.service.js
+++ services/user.service.js
@@ -31,19 +31,21 @@ export async function getUserListWithPagination(filter, options = {}) {
 export async function createUser(body) {
   const userData = await getOne({ mobileNumber: body.mobileNumber }, {});
 
+  let user;
   if (userData) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Mobile number already taken');
-  }
+    // throw new ApiError(httpStatus.BAD_REQUEST, 'Mobile number already taken');
+    if (body.nickName) {
+      const userDataForCheckNickName = await getOne({ nickName: body.nickName }, {});
+      if (userDataForCheckNickName) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'nick name is already present');
+      }
 
-  if (body.nickName) {
-    const userDataForCheckNickName = await getOne({ nickName: body.nickName }, {});
-
-    if (userDataForCheckNickName) {
-      throw new ApiError(httpStatus.BAD_REQUEST, 'nick name is already present');
+      // eslint-disable-next-line no-use-before-define
+      user = updateUser({ mobileNumber: body.mobileNumber }, body);
     }
+  } else {
+    user = await User.create(body);
   }
-
-  const user = await User.create(body);
   return user;
 }
 
@@ -52,9 +54,9 @@ export async function updateUser(filter, body, options = {}) {
   if (!userData) {
     throw new ApiError(httpStatus.NOT_FOUND, 'user not found');
   }
-  if (body.email && (await User.isEmailTaken(body.email, userData.id))) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
-  }
+  // if (body.email && (await User.isEmailTaken(body.email, userData.id))) {
+  //   throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+  // }
   const user = await User.findOneAndUpdate(filter, body, options);
   return user;
 }
